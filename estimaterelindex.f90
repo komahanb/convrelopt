656602b2be98778c349cea01bd3c1991658b10cc
komahan <komahan.cool@gmail.com>
2013-11-17 16:59:59 -0500

    cleaned up

--- estimaterelindex.f90
+++ estimaterelindex.f90
@@ -3,94 +3,68 @@ program main
   implicit none
 
   integer n,nmax,d
-  double precision tolerance,x(2),grad(2),hess(2,2),y(2),Ihess(2,2)
+  double precision tolerance,x(2),grad(2),hess(2,2),Ihess(2,2)
   double precision :: u(2)
   double precision :: beta,betaold,eps
   double precision :: g
 
   double precision :: sigmax(2),meanx(2)
   double precision :: val
-  
-  n=0 !iteration counter
+  integer::nvar,i
 
-!  tolerance=1.0D320	
 
-  eps=1.0d320
+  ! User input
 
-  nmax=1000	              	!MAXIMUM ITERATIONS
-
-  x=(/-0.8,0.8/)		!INITIAL GUESS
-
+  x=(/-0.8,0.8/) ! Initial point
+  nvar=2
   sigmax(1)=0.1
   sigmax(2)=0.1
 
   meanx(1)=-0.8
   meanx(2)=0.8
- 
 
-  do while ((eps.gt.1.0D-3).and.(n <= 2))
+  ! Stopping parameters
+  
+  tolerance=1.0D-10
+  nmax=1000 !set max iterations
 
-!!$
-!!$     if (n.ge.nmax) then
-!!$        if (residual>eps) then 
-!!$           print *,'Not converged after max iterations'
-!!$           stop
-!!$        end if
-!!$     end if
+  !Initialize
+  
+  n=0 !iteration counter
+  eps=1.0d320
+  beta=0.0
+  betaold=0.0
+
+  do while ((eps.gt.tolerance).and.(n <= nmax))
+
+     call get_G(nvar,x,g)    !get G value in x space
+
+     call get_dG(nvar,x,grad) ! get grad in x space
+     do i=1,nvar
+        grad(i)=grad(i)*sigmaX(i) ! convert grads to u space
+     end do
+
+     call transform(nvar,x,meanx,sigmax,x) ! map from x to u
+     val=(DOT_PRODUCT(grad,x)-g)/DOT_PRODUCT(grad,grad) ! in u space
      
-!     call transform(2,x,meanx,sigmax,u)
+     do i=1,nvar
+        u(i)=val*grad(i) ! in u space
+     end do
 
-     call get_G(2,x,g)    
-     print*,G,x,u
+     beta=dsqrt(DOT_PRODUCT(u,u)) ! in u space
 
-     call get_dG(2,x,grad)
-     print*,grad
-!     call transform(2,grad,meanx,sigmax,grad)
-!     print*,grad
-     call transform(2,x,meanx,sigmax,x)
-     print*,x
-     grad(1)=grad(1)*sigmaX(1)
-     grad(2)=grad(2)*sigmaX(2)
-
-!     print*,g
-  !   print*,grad
-!stop
-
- !    x(1)=(x(1)-(-0.8))/sigmax(1)
- !    x(2)=(x(2)-(0.8))/sigmax(2)
-!     print*,x
- !    stop
-
-     val=(DOT_PRODUCT(grad,x)-g)/DOT_PRODUCT(grad,grad)
-
-     y=val*grad
-
-     beta=dsqrt(DOT_PRODUCT(y,y))
-
-     print*,beta
-!     stop
-     eps=(beta-betaold)/beta
+     eps=abs((beta-betaold)/beta)
      betaold=beta
 
-     print*,y
-!stop
-     call invtransform(2,y,meanx,sigmax,x)
-     print*,x
-
-!     x=y
+     call invtransform(nvar,u,meanx,sigmax,x) ! map from u to x
      n=n+1
-
   end do
-
-  print*,"beta found:",beta,n+1
-
-!!$  WRITE(*,*)''
-!!$  WRITE(*,*)'%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-!!$  WRITE(*,*)''
-
-
-
-
+  print*,"eps:",eps
+  print*,"n :",n
+  print*,"b*:",beta
+  print*,"x*:",x
+  print*,"u*:",u
+  
 end program main
 !++++++++++++++++++++++++++++++++++++++++++++++++
 subroutine get_G(nvar,x,g)
@@ -133,7 +107,6 @@ subroutine transform(nvar,x,meanx,sigmax,u)
 
   return
 end subroutine transform
-
 !++++++++++++++++++++++++++++++++++++++++++++++++
 subroutine invtransform(nvar,u,meanx,sigmax,x)
   implicit none
@@ -148,42 +121,5 @@ subroutine invtransform(nvar,u,meanx,sigmax,x)
 
   return
 end subroutine invtransform
-
-
-!!$     WRITE(*,'(i4,3f15.4,3f15.4,3f15.4,3f15.4)') n,x(1),x(2),x(3),residual
-
-!     grad=(/18*x(1)+18*x(2)+x(3)*2*x(1)+2*x(3), 18*x(1)+26*x(2)+2*x(3)*x(2), x(1)**2+x(2)**2+2*x(1)-16/)
-
-!!$     hess(1,1)=18+2*x(3)
-!!$     hess(1,2)=18
-!!$     hess(1,3)=2*x(1)+2
-!!$
-!!$     hess(2,1)=18
-!!$     hess(2,2)=26+2*x(3)
-!!$     hess(2,3)=2*x(2)
-!!$
-!!$     hess(3,1)=2*x(1)+2
-!!$     hess(3,2)=2*x(2)
-!!$     hess(3,3)=0
-
-
-!!$     CALL FINDInv(hess,Ihess,3,errorflag)
-
-     
- !    y=x-matmul(ihess,grad)
-  !   x=y
-
-   !  residual=norm2(grad) 
-
-
-!!$
-!!$  WRITE(*,*)''
-!!$  WRITE(*,*)'%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-!!$  WRITE(*,*)'%%%%              CLASSICAL NEWTONS METHOD                        %%%%'
-!!$  WRITE(*,*)'%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-!!$  WRITE(*,*)''
-!!$  WRITE(*,*)'Iteration', '      X1','            X2','              X3','         Residual'
-!!$  WRITE(*,*)''
-!!$
-
+!++++++++++++++++++++++++++++++++++++++++++++++++
 
